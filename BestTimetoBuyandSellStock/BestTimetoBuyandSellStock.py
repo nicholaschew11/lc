4433ecda77bd9ffe10372db8981539b9ff856a38
n <chew.nicky@gmail.com>
2023-01-26 00:01:24 -0500

    01/26

---
+++ BestTimetoBuyandSellStock/BestTimetoBuyandSellStock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minBuy = prices[0]
+        maxProfit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < minBuy:
+                minBuy = prices[i]
+            elif prices[i] - minBuy > maxProfit:
+                maxProfit = prices[i] - minBuy
+        return maxProfit
