02062f7fec706abd4616675254c7814841af58cf
n <chew.nicky@gmail.com>
2023-03-03 01:50:43 -0500

    03/03

---
+++ LongestIncreasingSubsequence/LongestIncreasingSubsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        ans = 1
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+            ans = max(ans, dp[i])
+        return ans
