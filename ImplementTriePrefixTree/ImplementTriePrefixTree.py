61881608e6efbd516d0eb4d12ab1e70e9c0683d4
n <chew.nicky@gmail.com>
2023-03-07 00:51:00 -0500

    03/07

---
+++ ImplementTriePrefixTree/ImplementTriePrefixTree.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.completes_word = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node()
+            node = node.children[c]
+        node.completes_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.completes_word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
